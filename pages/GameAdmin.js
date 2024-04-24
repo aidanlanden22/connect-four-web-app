75bab47c38fd08caa984f34c7b699b89e02c3622
Aidan Landen <aidanlanden22@gmail.com>
2024-04-23 22:52:30 -0400

    added correct vercel domain

--- pages/GameAdmin.js
+++ pages/GameAdmin.js
@@ -1,15 +1,10 @@
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { v4 as uuid } from "uuid";
-import Link from "next/link";
-import { Didact_Gothic } from "next/font/google";
-import { useCookies } from "react-cookie";
 
-const DOMAIN =
-  "https://connect-four-web-3u2skgh4w-aidanlanden22s-projects.vercel.app/";
+const DOMAIN = "https://connect-four-web-app.vercel.app/";
 export default function GameAdmin() {
   const router = useRouter();
-  const [cookies, setCookie] = useCookies(["userId"]);
   const [gameId, setGameId] = useState(null);
   function generateGameId() {
     setGameId(uuid());
